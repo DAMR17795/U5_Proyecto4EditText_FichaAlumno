b6174687ad447b2eb4cba708d0caee233fbee186
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-11-14 13:37:12 +0100

    instituto

--- app/src/main/java/www/iesmurgi/u5_proyecto4edittext_fichaalumno/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/u5_proyecto4edittext_fichaalumno/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding.etEmail.inputType = InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
         binding.etAlergias.inputType = InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_FLAG_MULTI_LINE
         binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_PASSWORD
-
+        //hola
 
         setContentView(binding.root)
 
